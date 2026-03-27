6f9a11bab17caa02cb5e9d19f15ddbc0e6e41932
Oturpik <emmorychirchir@gmail.com>
2026-03-27 20:38:25 +0300

    Initial stable module release

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "min_size" {
+  type = number
+}
+
+variable "max_size" {
+  type = number
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "server_port" {
+  type    = number
+  default = 8080
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
